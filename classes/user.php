873bca6bc6e9c14da1958264845aceb8927eee73
nabixka <nabixka05@gmail.com>
2025-08-05 07:58:43 +0700

    update 1.1

--- classes/user.php
+++ classes/user.php
@@ -8,7 +8,7 @@ include("DB.php");
         }
 
         public function getById($id){
-             return $this->connect->query("SELECT * FROM user Where id = '$id'")->fetch_assoc();
+             return self::$connect->query("SELECT * FROM user Where id = '$id'")->fetch_assoc();
         }
 
         public function create($data){
@@ -16,45 +16,17 @@ include("DB.php");
             $username = $data['username'];
             $password = $data['password'];
 
-            return $this->connect->query("INSERT INTO user (name, username, password) VALUES ('$name', '$username', '$password')");
+            return self::$connect->query("INSERT INTO user (name, username, password) VALUES ('$name', '$username', '$password')");
         }
 
         public function updateName($id, $name, $username){
-            return $this->connect->query("UPDATE user SET name = '$name' username = '$username' WhERE id = '$id' ");
+            return self::$connect->query("UPDATE user SET name = '$name' username = '$username' WhERE id = '$id' ");
         }
 
         public function delete($id){
-            return $this->connect->query("DELETE FROM user WHERE id = '$id' ");
+            return self::$connect->query("DELETE FROM user WHERE id = '$id' ");
         }
 
     }
-
-   /* class User extends DB {
-
-    public function getAll() {
-        return self::$connect->query("SELECT * FROM user")->fetch_all(MYSQLI_ASSOC);
-    }
-
-    public function getById($id) {
-        return self::$connect->query("SELECT * FROM user WHERE id = '$id'")->fetch_assoc();
-    }
-
-    public function create($data) {
-        $name = $data['name'];
-        $username = $data['username'];
-        $password = $data['password'];
-        return self::$connect->query("INSERT INTO user (name, username, password) VALUES ('$name', '$username', '$password')");
-    }
-
-    public function updateName($id, $name) {
-        return self::$connect->query("UPDATE user SET name = '$name' WHERE id = '$id'");
-    }
-
-    public function delete($id) {
-        return self::$connect->query("DELETE FROM user WHERE id = '$id'");
-    }
-} */
-
-
 
 ?>
--- index.php
+++ index.php
@@ -8,14 +8,4 @@
     $data = $user->getAll();
     var_dump($data);
 
-
-
-    //DB::config("localhost", "root", "", "aut");
-    //var_dump(DB::connect());
-    //$user_model = new User();
-    //$users = getAll();
-    //$user = getById(1);
-
-
-
 ?>
